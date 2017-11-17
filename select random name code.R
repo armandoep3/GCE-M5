919adbe745b1b1b7426f4a41e17660c3a647f7e0
armandoep3 <33545299+armandoep3@users.noreply.github.com>
2017-11-17 17:18:46 +0100

    Add files via upload

---
+++ select random name code.R
@@ -0,0 +1,2 @@
+names <- c("akos", "alexander", "amanda", "anna", "asegid", "brenda", "cara", "claudia", "daria", "eva", "fabian", "fabrizzio", "Franziska", "georg", "HM", "jannis", "Karina", "kun", "leyla", "lisa", "luzie", "maike", "marit", "peter", "ram", "rebekka", "rickie", "ridwan", "ronja", "sarah", "thomas", "tina", "torben")
+sample(names, 1)
